3ea9a0774aba4d9587d17588bbb7cbe49ef167e4
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-10 17:44:38 +0200

    Aggiunte le classi per differenziare i risultati in pagina. Aggiunte classi background color a tutti e text center a tutti tranne ai numeri

--- js/script.js
+++ js/script.js
@@ -8,18 +8,23 @@ for(let i = 1; i <= 100; i++){
     const multipliCinque = (i % 5 == 0);
     let fizzBuzz = i
     let pElement = document.createElement("p");
+    
+    if(multipliTre && multipliCinque){
+        pElement.classList.add("total-black", "text-center")
+        console.log(fizzBuzz = "FizzBuzz")
+    } else if(multipliCinque){
+        pElement.classList.add("dark-blue", "text-center")
+        console.log(fizzBuzz = "Buzz")
+    } else if(multipliTre){
+        pElement.classList.add("light-blue", "text-center")
+        console.log(fizzBuzz = "Fizz")
+    } else {
+        pElement.classList.add("violet")
+        console.log(i)
+    }
+    
+    
     pElement.append(fizzBuzz);
     divElement.appendChild(pElement);
     
-    if(multipliTre && multipliCinque){
-        console.log(fizzBuzz = "FizzBuzz")
-    } else if(multipliCinque){
-        console.log(fizzBuzz = "Buzz")
-    } else if(multipliTre){
-        console.log(fizzBuzz = "Fizz")
-    } else {
-        console.log(i)
-    }
-
-
 }
