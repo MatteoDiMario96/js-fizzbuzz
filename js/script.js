87af726c0ce501070e6087481824c0db1564a140
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-10 17:14:16 +0200

    Creato l'if. Se sia multiplo di 3 che di 5 stampa FizzBuzz Se multiplo di 5 stampa Buzz Se mutiplo di 3 stampa Fizz

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 
 let limiteNumeri = 100;
 
-for ( let i = 1; i <= 100; i++){
-    console.log(i);
-
-    const multipliTre = (i % 3 == 0)
-    const multipliCinque = (i % 5 == 0)
+for(let i = 1; i <= 100; i++){
+    
+    const multipliTre = (i % 3 == 0);
+    const multipliCinque = (i % 5 == 0);
+    
+    if(multipliTre && multipliCinque){
+        let fizzBuzz = i
+        console.log(fizzBuzz = "FizzBuzz")
+    } else if(multipliCinque){
+        console.log(fizzBuzz = "Buzz")
+    } else if(multipliTre){
+        console.log(fizzBuzz = "Fizz")
+    } else {
+        console.log(i)
+    }
+    
 }
